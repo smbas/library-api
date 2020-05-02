e25bcd0e093fed8f8abbaa84583204ce3ea63816
Stanislav Bas <stanislav.m.bas@gmail.com>
2020-05-02 14:26:05 +0300

    Initial project setup

---
+++ src/settings.py
@@ -0,0 +1,12 @@
+DOMAIN = {
+    "people": {
+
+    }
+}
+
+MONGO_HOST = "mongo"
+# MONGO_PORT = 27017
+# MONGO_USERNAME = ""
+# MONGO_PASSWORD = ""
+# MONGO_AUTH_SOURCE = ""
+MONGO_DBNAME = "apitest"
